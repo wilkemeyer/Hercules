b31e1b21da0bc03d265fbcdac4b19e97e6151e84
Florian Wilkemeyer <fw@f-ws.de>
2015-12-15 19:05:39 +0100

    Applied correct License (MIT) to rgCore Components

--- src/rgcore/roalloc.cpp
+++ src/rgcore/roalloc.cpp
@@ -1,17 +1,27 @@
-/**
- * Copyright (c) 2012, Florian Wilkemeyer <fw@f-ws.de>
- *
- * Use, distribution and modification of this source code and/or
- * resulting object code is not permitted. 
- * Expecting non-comercial usage of the resulting binary / object code.
- * All other use, distribution or modification is prohibited 
- * without written permission from Florian Wilkemeyer
- *
- * Contact Information
- *  E-Mail: fw@f-ws.de
- * 
- * This File is part of Ragnarok Online Client (roclient)
- */
+/*
+	The MIT License (MIT)
+
+	Copyright (c) 2015 Florian Wilkemeyer <fw@f-ws.de>
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+*/
+
 #include "stdafx.h"
 
 #if LINUX | ROALLOC_USE_TBB
